37758df25d5d7049d6fb60a34535c7d9519b4bd8
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2023-09-29 12:38:35 -0700

    added react-frontend

---
+++ react-frontend/README.md
@@ -0,0 +1,2 @@
+# csc307-react-app
+This is a React App created for CSC 307.
---
+++ react-frontend/src/MyApp.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Table from "./Table";
+
+function MyApp() {
+    
+    const [characters, setCharacters] = useState([]);
+
+    function removeOneCharacter (index) {
+        const updated = characters.filter((character, i) => {
+            return i !==index
+        });
+        setCharacters(updated);
+    }    
+    
+    return (
+        <div className="container">
+            <Table characterData={characters}
+                    removeCharacter={removeOneCharacter}/>
+        </div>
+    );
+}
+export default MyApp;
