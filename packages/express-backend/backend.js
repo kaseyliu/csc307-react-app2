e4706596330b801f9101ffaac09776f79c682dba
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2023-10-04 09:53:43 -0700

    added find user by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,17 +38,31 @@ const users = {
         .filter( (user) => user['name'] === name); 
 }
 
-app.get('/users', (req, res) => {
-    const name = req.query.name;
-    if (name != undefined){
-        let result = findUserByName(name);
-        result = {users_list: result};
+const findUserById = (id) =>
+    users['users_list']
+        .find((user) => user['id'] === id)
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
         res.send(result);
     }
-    else{
-        res.send(users);
-    }
 });
+
+// app.get('/users', (req, res) => {
+//     const name = req.query.name;
+//     if (name != undefined){
+//         let result = findUserByName(name);
+//         result = {users_list: result};
+//         res.send(result);
+//     }
+//     else{
+//         res.send(users);
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
